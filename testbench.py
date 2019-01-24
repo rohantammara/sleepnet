06b8a27897881a8e4f949b5d9ceb12cc9179412f
Rohan-o-Tammara <ta.rohan550@gmail.com>
2019-01-24 11:10:00 +0530

    added best case

--- testbench.py
+++ testbench.py
@@ -7,8 +7,10 @@ TRIALS = 10
 print("\nStarting test (" + str(TRIALS) + " trials)\n")
 
 start = time()
-loss = 0
-acc = 0
+avg_loss = 0
+avg_acc = 0
+best_loss = 0
+best_acc = 0
 
 x,y,xt,yt = prepare_data()
 
@@ -16,12 +18,18 @@ classifier = load_model('saved_models/psg_model_2.h5')
 
 for _ in range(TRIALS):
     scores = classifier.evaluate(xt,yt,BATCH_SIZE,verbose=0)
-    loss += scores[0]
-    acc += scores[1]
+    if scores[0] > best_loss:
+        best_loss = scores[0]
+    if scores[1] > best_acc:
+        best_acc = scores[1]
+    avg_loss += scores[0]
+    avg_acc += scores[1]
 
-loss = loss/TRIALS
-acc = acc/TRIALS
+avg_loss = avg_loss/TRIALS
+avg_acc = avg_acc/TRIALS
 
-print("\nLoss: ", loss)
-print("Accuracy: ", acc)
+print("\nBest Loss: ", best_loss)
+print("Best Accuracy: ", best_acc)
+print("Mean Loss: ", avg_loss)
+print("Mean Accuracy: ", avg_acc)
 print("\nfinished in " + str(time()-start) + " seconds")
